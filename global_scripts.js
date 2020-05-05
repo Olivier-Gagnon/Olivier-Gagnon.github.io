15eee998cc957fe8699c2421bad5194c7b7b18a1
OlivierG <olive.gagnon@gmail.com>
2020-05-04 21:33:56 -0500

    added plotly depedency and removed unused js

--- global_scripts.js
+++ global_scripts.js
@@ -2,14 +2,15 @@ $(document).ready(function () {
 
     // Handle Navbar
     $('#navbar-container').load("/components/navbar/navbar.html", function () {
-        $.getScript("/components/navbar/navbar.js")});
+        $.getScript("/components/navbar/navbar.js")
+    });
 
     // Handle HomePage
     $('#section-container').load("/sections/home/home.html", function () {
-        $.getScript("/sections/home/home.js")});
+    });
 
     // Handle Footer
     $('#footer-container').load("/components/footer/footer.html", function () {
-        $.getScript("/components/footer/footer.js")});
+    });
 
 });
